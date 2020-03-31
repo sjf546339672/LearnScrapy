e0c8a21e92d85401e8fee21e0506bb88a933000e
shijf <shijf@broada.com>
2020-03-31 18:26:01 +0800

    te

--- Book1/demo1.py
+++ Book1/demo1.py
@@ -1,51 +1,35 @@
 # coding: utf-8
-import re
 import urllib
-from urllib import request
+import urllib.request as urllib2
 from urllib.error import URLError
 
+import re
 
-def download(url, user_agent='wswp',num_retries=2):
+
+def download(url, num_retries=2, user_agent="wswp"):
+    print("douwnload:{}".format(url))
+    headers = {"User-agent": user_agent}
+    request = urllib2.Request(url, headers=headers)
     try:
-        html = request.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except URLError as e:
-        print(e.reason)
+        print("download error", e.reason)
         html = None
         if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, user_agent, num_retries - 1)
+            if (hasattr(e, "code")) and 500 <= e.code < 600:
+                return download(url, num_retries-1)
     return html
 
 
 def crawl_sitemap(url):
     sitemap = download(url)
-    links = re.findall('<loc>(.*?)</loc>', sitemap)
-    for i in links:
-        html = download(i)
+    links = re.findall("<loc>(.*?)</loc>", sitemap)
+    for link in links:
+        html = download(link)
         print(html)
 
-# t = crawl_sitemap('http://example.webscraping.com/sitemap.xml')
-# print(t)  # http://example.webscraping.com/view/1
 
-
-
-# http://example.num_webscraping.com/places/default/view/5
-import itertools
-
-max_errors = 5
-num_errors = 0
-
-for page in itertools.count(1):
-    url = 'http://example.webscraping.com/places/default/view/{}'.format(page)
-    print(url)
-    html = download(url)
-    if html is None:
-        num_errors += 1
-        if num_errors == max_errors:
-            break
-        else:
-            print(html)
-            num_errors = 0
+print(crawl_sitemap("http://example.webscrping.com/sitemap.xml"))
 
 
 
